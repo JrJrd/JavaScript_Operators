6ecb32bcd1ccd465efa9e689fdcdad5e8044976f
seaer <jrd.seaer@gmail.com>
2023-08-30 05:01:38 +0300

    operators testing

--- app.js
+++ app.js
@@ -6,14 +6,49 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let sum = a + b;
+let multiply = a * b;
+let divid = a / b;
+let remainder = a % b;
+console.log(sum);
+console.log(multiply);
+console.log(divid);
+console.log(remainder);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+let value1 = num + str;
+let value2 = num + str2;
+let value3 = num + isPresent;
+let value4 = firstName + num;
+let value5 = isPresent + str;
+let value6 = firstName + lastName;
+
+console.log(value6 + " " + value1 + " " + value2 + " " + value3 + " " + value4 + " " + value5);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(val == str3); //true
+console.log(!isPresent2); //true
+console.log(!isPresent2 || isPresent2); //true
+console.log(0 == false); //true
+console.log(0 === false); //false
+console.log(0 != false); //false
+console.log(0 !== false); //true
+
